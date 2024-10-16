5ba08b4fb94a56ffc322a768eaf0c6833f75a172
Ana Luiza <160125249+analuizab3@users.noreply.github.com>
2024-10-16 16:25:08 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+export default function App() {
+        return (
+          <div className="App">
+            <h1> React</h1>
+            <p>A blibioteca para interfaces de usuários web e nativas.</p>
+
+          <button> Aprenda React </button>
+          <button> Referência de API </button>
+          <p>______________________________________________</p>
+            <h1>Crie interfaces de usuário de componentes</h1>
+            <p>React permite que você construa interfaces de usuários a partir de pedaços individuais chamados componentes.</p>
+            <p>______________________________________________</p>
+            <h1>Escreva componentes com código e marcação</h1>
+            <p>Componentes React são funções JavaScript.A sintaxe de marcação é chamada de JSX,É uma extenção de sintaxe JavaScrip popularizada pelo React.</p>
+            <p>______________________________________________</p>
+            <h1> Próximos passos</h1>
+            <ul>
+              <li>Uso de dados dinâmicos no JSX</li>
+              <li>Criação de novos componentes</li>
+              <li> Estilização de componentes</li>
+              <li>Reutilização de componentes</li>
+              <li>Uso de props e children</li>
+              <li>Uso de eventos do JavaScrip</li>
+            </ul>
+          </div>
+        )
+      }
+      
